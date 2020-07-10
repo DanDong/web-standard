5d456bc38682bb8a9d4b069289a40f4428ba9502
wudandong <1054523482@qq.com>
2020-07-10 10:01:47 +0800

    完善

--- public/config.js
+++ public/config.js
@@ -1,5 +1,17 @@
 var config = {
-  host: 'http://127.0.0.1:3000',
-  title: '七月'
+  host: 'http://127.0.0.1:8088',
+  title: '七月',
+  iconfont: [
+    {
+      url: 'http://127.0.0.1:8088/iconfont/01/index.html',
+      title: '第一个图标文件',
+      code: 'first'
+    },
+    {
+      url: 'http://127.0.0.1:8088/iconfont/01/index.html',
+      title: '第二个图标文件',
+      code: 'sencod'
+    }
+  ]
 }
 document.title = config.title
